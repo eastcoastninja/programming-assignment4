ac96b8649f257e3eaffbfcc5e755d2da06c65c56
Julian Rios <julian1rios@gmail.com>
2017-11-07 20:27:49 -0500

    registrar class added needs testing

---
+++ registrar.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+/**
+ * This registrar class represents a registrar in a educational system.
+ * This class hold the details of a registrar.
+ *
+ * @author Julian Rios
+ * @version 2017.11.07
+ */
+ public class registrar
+ {
+   // collection of courses offered
+   private ArrayList<Course> courses;
+
+   /**
+    * Create a new registrar with an empty collection of courses.
+    */
+   public registrar()
+   {
+     courses = new ArrayList<>();
+   }
+
+   /**
+   * Add a course to the collection.
+   */
+   public void addCourse(Course newCourse)
+   {
+     course.add(newCourse);
+   }
+
+   /**
+   * Remove a course from the collection.
+   */
+   public void addStudent(Course courseToRemove)
+   {
+     course.remove(courseToRemove);
+   }
+
+   /**
+   * Print a list of all the courses offered.
+   */
+   public void printList()
+   {
+
+   }
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
